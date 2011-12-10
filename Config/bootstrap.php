003e763a3c5ed5cb2f82c785fc76013015c8e52d
Matus Sabo <lamanabie@gmail.com>
2011-12-11 00:08:20 +0100

    Adding cakephp plugin files

---
+++ Config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+Configure::write('Recaptcha.publicKey', '6LfDCMsSAAAAANafm_TCMjDPrq7Z_4GU9j6Xt2z_');
+Configure::write('Recaptcha.privateKey', '6LfDCMsSAAAAAMjzs7gTMXmONcDnt41D5270TuHB');
+?>
